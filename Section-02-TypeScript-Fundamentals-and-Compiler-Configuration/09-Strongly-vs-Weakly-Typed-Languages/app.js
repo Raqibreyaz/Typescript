e2b31d7818fe18dfe115f2f3417692cb79a3883c
Raquib Reyaz <raquibreyaz111@gmail.com>
2026-06-18 23:55:15 +0530

    learnt about strongly and weakly typed languages

---
+++ Section-02-TypeScript-Fundamentals-and-Compiler-Configuration/09-Strongly-vs-Weakly-Typed-Languages/app.js
@@ -0,0 +1,12 @@
+let num = 5;
+let str = "2";
+
+console.log(num - str); //3
+console.log(num + str); //52
+console.log(num * str); //10
+console.log(num / str); //2.5
+console.log("hi" / 5); //NaN
+console.log(5 + []); //5
+console.log(5 + {}); //5[object object]
+console.log(5 + ''); //5
+
